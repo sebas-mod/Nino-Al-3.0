e0919af1b9517dcea0c5b717ba38476e556eb215
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 16:14:04 -0300

    Update tebakgambar.js

--- plugins/game/tebakgambar.js
+++ plugins/game/tebakgambar.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebakgambar', {
-    alias: ['tg', 'guessimage'],
+    alias: ['tg', 'adivinajuego', 'imagen', 'adivinanimagen'],
     emoji: '🖼️',
-    title: 'TEBAK GAMBAR',
-    description: 'Tebak kata dari gambar',
+    title: 'ADIVINA LA IMAGEN',
+    description: 'Adivina la palabra a partir de la imagen',
     timeout: 90000,
     hasImage: true,
     questionField: null,
