59339f7444aec550394de26b7a112f15c8bba284
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 16:24:33 -0300

    Update game aliases and titles for tebaklagu

--- plugins/game/tebaklagu.js
+++ plugins/game/tebaklagu.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebaklagu', {
-    alias: ['tl', 'guesssong'],
+    alias: ['tl', 'adivinalacancion', 'cancion', 'musica'],
     emoji: '🎵',
-    title: 'TEBAK LAGU',
-    description: 'Tebak judul lagu'
+    title: 'ADIVINA LA CANCIÓN',
+    description: 'Adivina el título de la canción'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('tebaklagu')
