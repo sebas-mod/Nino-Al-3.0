4c09251da8ecf22eebbe0ab3a96ff7b789b9241a
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 16:06:18 -0300

    Update game alias and titles in susunkata.js

--- plugins/game/susunkata.js
+++ plugins/game/susunkata.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('susunkata', {
-    alias: ['susun', 'scramble'],
+    alias: ['susun', 'ordenarpalabra', 'ordenar', 'scramble'],
     emoji: '🔠',
-    title: 'SUSUN KATA',
-    description: 'Susun kata dari huruf'
+    title: 'ORDENAR PALABRA',
+    description: 'Ordena las letras para formar la palabra correcta'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('susunkata')
