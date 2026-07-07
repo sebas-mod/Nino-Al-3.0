da99fae2e35e5210c5b73d87c71df41e4a632462
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 16:26:14 -0300

    Rename game and update aliases in tebaklirik

--- plugins/game/tebaklirik.js
+++ plugins/game/tebaklirik.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebaklirik', {
-    alias: [],
+    alias: ['adivinalaletra', 'letra', 'letras'],
     emoji: '🎤',
-    title: 'TEBAK LIRIK',
-    description: 'Tebak lirik lagu'
+    title: 'ADIVINA LA LETRA',
+    description: 'Adivina la letra de la canción'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('tebaklirik')
