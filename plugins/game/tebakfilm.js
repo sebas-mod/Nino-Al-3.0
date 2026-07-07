d9f11419633f223ce8f363f5f00221237e62a29f
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 16:11:19 -0300

    Update game aliases and titles for tebakfilm

--- plugins/game/tebakfilm.js
+++ plugins/game/tebakfilm.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebakfilm', {
-    alias: ['tf', 'guessmovie'],
+    alias: ['tf', 'adivinafilm', 'pelicula'],
     emoji: '🎬',
-    title: 'TEBAK FILM',
-    description: 'Tebak judul film'
+    title: 'ADIVINA LA PELÍCULA',
+    description: 'Adivina el título de la película'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('tebakfilm')
