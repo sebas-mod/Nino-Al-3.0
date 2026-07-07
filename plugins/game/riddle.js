0c73ac285a0f59f9464f329e576f271094c65b19
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 16:02:26 -0300

    Update riddle game aliases and titles to Spanish

--- plugins/game/riddle.js
+++ plugins/game/riddle.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('riddle', {
-    alias: ['rd', 'tebaktebak', 'riddles'],
+    alias: ['rd', 'acertijo', 'acertijos', 'adivinanza', 'adivinanzas'],
     emoji: '❓',
-    title: 'RIDDLE',
-    description: 'Riddle/tebak-tebakan'
+    title: 'ACERTIJO',
+    description: 'Adivinanzas y acertijos'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('riddle')
