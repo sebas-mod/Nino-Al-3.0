4052ac56d4319e08001940d634d02298b61767bd
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-07-07 15:51:44 -0300

    Update game aliases and localization for asahotak

--- plugins/game/asahotak.js
+++ plugins/game/asahotak.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('asahotak', {
-    alias: ['asah', 'quiz'],
+    alias: ['asah', 'quiz', 'trivia', 'cerebro'],
     emoji: '🧠',
-    title: 'ASAH OTAK',
-    description: 'Game asah otak - tebak jawaban'
+    title: 'ACERTIJO MENTAL',
+    description: 'Juego mental - ¡adivina la respuesta correcta!'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('asahotak')
